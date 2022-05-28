52e1882131ae4ac37040f20617ce3aa60a3383b6
ViYarem <68001529+ViYarem@users.noreply.github.com>
2022-05-28 18:19:44 +0300

    Add files via upload
    automatical installing PyPDF2 and FPDF

---
+++ installer_packages_PyPDF2andFPDF.py
@@ -0,0 +1,9 @@
+import pip
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+if __name__ == '__main__':
+    install('PyPDF2')
+    install('fpdf')
